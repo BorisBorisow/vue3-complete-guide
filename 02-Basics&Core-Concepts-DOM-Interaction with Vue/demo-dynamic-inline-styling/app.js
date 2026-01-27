836093b89e314822c9ea956efc0c1b3edbb5fbb7
BorisBorisowgit config --globa user.email bborrisow@gmail.com <bborrisow@gmail.com>
2026-01-27 19:53:40 +0100

    demo dynamic inline styling

---
+++ 02-Basics&Core-Concepts-DOM-Interaction with Vue/demo-dynamic-inline-styling/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxCSelected: false,
+            boxBSelected: false,
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            if (box === "A") {
+                this.boxASelected = !this.boxASelected;
+            } else if (box === "B") {
+                this.boxBSelected = !this.boxBSelected;
+            } else if (box === "C") {
+                this.boxCSelected = !this.boxCSelected;
+            }
+        }
+    }
+})
+
+app.mount('#styling')
